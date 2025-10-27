489f152e71af8fdc2b91980f6f55667276cd27ef
JCR2005 <juanrodriguez202231970@cunoc.edu.gt>
2025-10-26 23:43:07 -0600

    probando prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://torchless-unpiteously-raphael.ngrok-free.dev'
+};
--- src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  production: true,apiUrl: 'https://torchless-unpiteously-raphael.ngrok-free.dev'
-};
